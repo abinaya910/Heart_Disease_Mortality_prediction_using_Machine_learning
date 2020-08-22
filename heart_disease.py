1e3662b5cd849c9477c38bee700932c68b86a38f
abinaya910 <68158322+abinaya910@users.noreply.github.com>
2020-08-22 10:25:19 +0530

    Update heart_disease.py

--- heart_disease.py
+++ heart_disease.py
@@ -1,26 +1,55 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import math
+from lightgbm import LGBMRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import confusion_matrix
+def evaluate(x_train, x_eval, y_train, y_eval, model):
+    model.fit(x_train, y_train)
+    y_predict = model.predict(x_eval)
+    return math.sqrt(mean_squared_error(y_predict, y_eval))
 
-#importing dataset
-dataset=pd.read_csv('C:/Users/Desktop/IIT Roorkee/Training_dataset.csv')
+# Read data
+df_X_train = pd.read_csv('Trainvaluesmean.csv')
+df_Y_train = pd.read_csv('Training_labels.csv')
+df_X_test  = pd.read_csv('Test_values.csv')
 
-x=dataset.iloc[:,:-1].values
-y=dataset.iloc[:,-1].values
+df_X_train.describe()
 
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values='NaN',strategy='mean',axis=0)
-imputer=imputer.fit(x[:,:])
-x[:,:]=imputer.transform(x[:,:])
-x_train,x_test,y_train, y_test=train_test_split(x,y,test_size=0.75,random_state=0)
+# Clean missing data , Convert categorical features
+ID = 'row_id'
+target = 'heart_disease_mortality_per_100k'
 
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X=LabelEncoder()
-x[:,1]=labelencoder_X.fit_transform(x[:,1])
-onehotencoder=OneHotEncoder(categorical_features=[1])
-x=onehotencoder.fit_transform(x).toarray()
-labelencoder_y=LabelEncoder()
-y=labelencoder_y.fit_transform(y)
+X_train = pd.get_dummies(df_X_train.drop(ID, axis=1).fillna(0)).values
+Y_train = df_Y_train[target].values
+X_test  = pd.get_dummies(df_X_test.drop(ID,axis=1).fillna(0)).values
 
-print(x)
+# Split training data , Evaluate model , Tune hyper-parameters
+x_train, x_eval, y_train, y_eval = train_test_split(X_train, Y_train, test_size=0.3, random_state=1)
+
+
+
+#preprocessing
+from sklearn.preprocessing import StandardScaler as ss
+sc = ss()
+x_train = sc.fit_transform(x_train)
+x_eval = sc.transform(x_eval)
+
+
+
+model = LGBMRegressor(n_estimators=1000, learning_rate=0.05, num_leaves=30)
+rmse = evaluate(x_train, x_eval, y_train, y_eval, model)
+print ('LGBMRegressor RMSE :', rmse)
+
+
+
+# Train model , Predict
+model.fit(X_train, Y_train)
+Y_predict = model.predict(X_test)
+
+# Submit
+submit = np.column_stack((df_X_test[ID], np.round(Y_predict)))
+np.savetxt("submit.csv", submit, fmt='%i', delimiter=",", header=ID+','+target, comments='')
+#cm_test = confusion_matrix(y_train, y_eval)
+print (Y_predict)
